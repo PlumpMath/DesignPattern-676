32dca83cb6aedb035efd500d8eb249f53cadcf7f
庞东明 <pangdongming@zhite.com>
2017-01-20 09:21:19 +0800

    add more patterns

---
+++ src/businessDelegate/BusinessLookup.java
@@ -0,0 +1,11 @@
+package businessDelegate;
+
+public class BusinessLookup {
+	public BusinessService getBusinessService(String serviceType) {
+		if (serviceType.equalsIgnoreCase("EJB")) {
+			return new EJBService();
+		} else {
+			return new JMSService();
+		}
+	}
+}
---
+++ src/template/Game.java
@@ -0,0 +1,16 @@
+package template;
+
+public abstract class Game {
+	abstract void initialize();
+	abstract void startPlay();
+	abstract void endPlay();
+	
+	// template method
+	public final void play() {
+		// initialize the game
+		initialize();
+		
+		startPlay();
+		endPlay();
+	}
+}
